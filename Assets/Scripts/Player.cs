6cc64eb7221bdcf484f56c461e67be07ab0f4593
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-09 00:47:48 +1100

    Add `Player.inventory`

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,10 @@
-﻿using UnityEngine;
+﻿using Schwer.ItemSystem;
+using UnityEngine;
 
 public class Player : Character {
+    [SerializeField] private InventorySO _inventory = default;
+    public Inventory inventory => _inventory.value;
+
     protected override void GetInput() {
         input.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
