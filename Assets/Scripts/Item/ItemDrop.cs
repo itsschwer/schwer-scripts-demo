13173c35d5b46358b8eafc3b1f1a4786313b8835
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-10 17:24:06 +1100

    Implement item drops

---
+++ Assets/Scripts/Item/ItemDrop.cs
@@ -0,0 +1,14 @@
+﻿using Schwer.ItemSystem;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour {
+    [SerializeField] private Item item = default;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        var player = other.GetComponent<Player>();
+        if (player != null) {
+            player.inventory[item]++;
+            Destroy(this.gameObject);
+        }
+    }
+}
