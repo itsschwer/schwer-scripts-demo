d4fb21301cf5071cb443df2dc3bddb41e9776c46
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-15 13:06:08 +1100

    Update InventoryDrawer.cs
    - Remove labels for keys and values.
    - Remove indentation modification.
    - Add warning box in case number of keys != number of values

--- Assets/SchwerScripts/ItemSystem/Editor/InventoryDrawer.cs
+++ Assets/SchwerScripts/ItemSystem/Editor/InventoryDrawer.cs
@@ -1,33 +1,32 @@
 ﻿using Schwer.ItemSystem;
-using SchwerEditor;
 using UnityEditor;
 using UnityEngine;
 
 [CustomPropertyDrawer(typeof(Inventory))]
 public class InventoryDrawer : PropertyDrawer {
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-        // EditorGUI.BeginProperty(position, label, property);
-
         var keys = property.FindPropertyRelative("keys");
         var values = property.FindPropertyRelative("values");
 
-        EditorGUILayout.BeginHorizontal();
-        property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, "Contents (" + keys.arraySize + ")");
-        EditorGUILayout.EndHorizontal();
+        if (keys.arraySize != values.arraySize) {
+            var warning = "The number of keys does not match the number of values!";
+            var details = $"({keys.arraySize} keys; {values.arraySize} values)";
+            EditorGUILayout.HelpBox(warning + "\n" + details, MessageType.Warning);
+        }
+
+        property.isExpanded = EditorGUILayout.Foldout(property.isExpanded, "Contents (" + keys.arraySize + ")", true);
 
         if (property.isExpanded) {
-            EditorGUI.indentLevel++;
             EditorGUI.BeginDisabledGroup(true);
             for (int i = 0; i < keys.arraySize; i++) {
                 EditorGUILayout.BeginHorizontal();
                 var key = keys.GetArrayElementAtIndex(i);
                 var value = values.GetArrayElementAtIndex(i);
-                EditorGUILayout.PropertyField(key);
-                EditorGUILayout.PropertyField(value);
+                EditorGUILayout.PropertyField(key, GUIContent.none);
+                EditorGUILayout.PropertyField(value, GUIContent.none);
                 EditorGUILayout.EndHorizontal();
             }
             EditorGUI.EndDisabledGroup();
-            EditorGUI.indentLevel--;
         }
     }
 }
