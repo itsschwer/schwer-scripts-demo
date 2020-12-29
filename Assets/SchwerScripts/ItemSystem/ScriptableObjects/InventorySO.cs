51b0e893d50df4236060aaf391519ea8dedc0e00
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-12-29 15:37:39 +1100

    Mark InventorySO as dirty (editor only) to allow editor saving

--- Assets/SchwerScripts/ItemSystem/ScriptableObjects/InventorySO.cs
+++ Assets/SchwerScripts/ItemSystem/ScriptableObjects/InventorySO.cs
@@ -4,5 +4,14 @@ namespace Schwer.ItemSystem {
     [CreateAssetMenu(menuName = "Scriptable Object/Item System/Inventory")]
     public class InventorySO : ScriptableObject {
         public Inventory value = new Inventory();
+
+#if UNITY_EDITOR
+        private void OnEnable() => value.OnContentsChanged += MarkDirtyIfChanged;
+        private void OnDisable() => value.OnContentsChanged -= MarkDirtyIfChanged;
+
+        private void MarkDirtyIfChanged(Item item, int count) {
+            UnityEditor.EditorUtility.SetDirty(this);
+        }
+#endif
     }
 }
